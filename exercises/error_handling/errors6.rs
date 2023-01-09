c23c039a561891f9b789b275dbd43e398e97151d
Zac Hooper <zac.g.hooper@gmail.com>
2023-01-10 09:39:01 +1100

    Finished errors

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,8 +8,6 @@
 
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -25,13 +23,26 @@ impl ParsePosNonzeroError {
     }
     // TODO: add another error conversion function here.
     // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+
+    // Check that it's an actual number here. If not then raise Parsing Error
+    // Use ? to throw the error early
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
+
+    // // Could also achieve the same with match like below
+    // let x: i64 = match s.parse() {
+    //     Ok(num) => num,
+    //     Err(num) => return Err(ParsePosNonzeroError::ParseInt(num)),
+    // };
+
+    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation) // Check that it's a Positive non zero number. If not then raise a Non zero error.
 }
 
 // Don't change anything below this line.
